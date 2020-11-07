d014308c33ddeafcc08b05c9ef94de3bbb8cc414
iavivai <hello@yukitakaiha.com>
2020-11-07 17:35:14 +0900

    Created posts list.

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export default function Post({ postData }) {
+  return (
+    // TODO
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <main>
+        <h1>{postData.title}</h1>
+        <div>
+          <p>{postData.date}</p>
+        </div>
+        <img
+          src={`/images/${postData.image}`}
+          alt={postData.title}
+        />
+      </main>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
