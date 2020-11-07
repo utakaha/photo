739ca4f1d90ed89388611db2ddd402526d2f3635
iavivai <hello@yukitakaiha.com>
2020-11-07 18:54:25 +0900

    Fix Layout component.

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,15 @@
-function Layout({ children }) {
-  return <div>{children}</div>
-}
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
-export default Layout
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+      </Head>
+
+      <main>{children}</main>
+    </div>
+  )
+}
